eac17f80daccb61981d453ea471d52240cd36ba1
dattq2004 <0901dattran@gmail.com>
2025-02-13 00:36:34 +0000

    update product model

--- src/java/model/Product.java
+++ src/java/model/Product.java
@@ -4,10 +4,79 @@
  */
 package model;
 
+import java.util.Date;
+
 /**
  *
  * @author LAPTOP ASUS
  */
 public class Product {
+    private int id;
+    private String name;
+    private double price;
+    private String description;
+    private int stock;
+    private Date import_date;
+
+    public Product() {
+    }
+
+    public Product(int id, String name, double price, String description, int stock, Date import_date) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.description = description;
+        this.stock = stock;
+        this.import_date = import_date;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public Date getImport_date() {
+        return import_date;
+    }
+
+    public void setImport_date(Date import_date) {
+        this.import_date = import_date;
+    }
+    
     
 }
